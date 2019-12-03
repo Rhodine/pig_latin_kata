99f560788fa376ba6cf2a4d73a85e3b8d62d494c
Rhodine <rorleans_lindsay@yahoo.co.uk>
2019-12-03 15:54:34 +0000

    Exercise 4

--- exercise_4.rb
+++ exercise_4.rb
@@ -5,7 +5,42 @@
 # Assume words either start with a capital letter or don't — don't worry about
 # other formats
 
+word = " "
+first_letter = " "
+choice = " "
 def pig_latin(word)
+  choice1 = word[0] == word[0].upcase
+  if choice1 == true
+    choice = 1
+  else
+    choice = 0
+  end
+
+
+
+  if word[0] == "q" && word[1] == "u"
+    word.slice!(0)
+    word.slice!(0)
+    word + "quay"
+
+  elsif word[0] == "a" || word[0] == "e" || word[0] == "i" || word[0] == "o" || word[0] == "u"
+     word = word + "way"
+
+  else
+    first_letter = word[0]
+    while first_letter != "a" and word[0] != "e" and word[0] != "i" and word[0] != "o" and word[0] != "u"
+      word.slice!(0)
+      word = word + first_letter
+      first_letter = word[0]
+    end
+      word = word + "ay"
+  end
+
+  if choice == 1
+    word.capitalize
+  elsif choice == 0
+    word.downcase
+  end
 
 end
 
