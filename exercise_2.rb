45b6a169ed393e80c5f108ce0f7a2f2b0a6a720e
Rhodine <rorleans_lindsay@yahoo.co.uk>
2019-12-03 15:16:23 +0000

    Exercise 2

--- exercise_2.rb
+++ exercise_2.rb
@@ -6,9 +6,22 @@
 #      sprinkle -> inklespray
 #
 # Still assume the words are lowercase.
-
+word = " "
+first_letter = " "
 def pig_latin(word)
 
+  if word[0] == "a" || word[0] == "e" || word[0] == "i" || word[0] == "o" || word[0] == "u"
+     word + "way"
+
+  else
+    first_letter = word[0]
+    while first_letter != "a" and word[0] != "e" and word[0] != "i" and word[0] != "o" and word[0] != "u"
+      word.slice!(0)
+      word = word + first_letter
+      first_letter = word[0]
+    end
+      word + "ay"
+  end
 end
 
 ## Tests:
