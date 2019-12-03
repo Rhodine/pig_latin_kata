5812898e99ac7ad8e5b0fabd5179f4dc48a29b38
Rhodine <rorleans_lindsay@yahoo.co.uk>
2019-12-03 16:26:30 +0000

    Exercise 5

--- exercise_5.rb
+++ exercise_5.rb
@@ -2,8 +2,50 @@
 # e.g. "The quick fox jumped over the lazy dog" ->
 #      "Ethay ickquay oxfay umpedjay overway ethay azylay ogday"
 
+word = " "
+first_letter = " "
+choice = " "
+array1 = []
+
 def pig_latin_sentence(sentence)
 
+  array1 = sentence.split(" ")
+  array2 = array1.map do |word|
+
+    choice1 = word[0] == word[0].upcase
+    if choice1 == true
+      choice = 1
+    else
+      choice = 0
+    end
+
+
+
+    if word[0] == "q" && word[1] == "u"
+      word.slice!(0)
+      word.slice!(0)
+      word = word + "quay"
+
+    elsif word[0] == "a" || word[0] == "e" || word[0] == "i" || word[0] == "o" || word[0] == "u"
+      word = word + "way"
+
+    else
+      first_letter = word[0]
+      while first_letter != "a" and word[0] != "e" and word[0] != "i" and word[0] != "o" and word[0] != "u"
+        word.slice!(0)
+        word = word + first_letter
+        first_letter = word[0]
+      end
+        word = word + "ay"
+      end
+
+      if choice == 1
+        word.capitalize
+      elsif choice == 0
+        word.downcase
+      end
+    end
+    array2.join(" ")
 end
 
 ## Tests:
