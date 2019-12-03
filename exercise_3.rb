dc5be92634089c8ded3c7a6cdc3f4ce8b6df3b84
Rhodine <rorleans_lindsay@yahoo.co.uk>
2019-12-03 15:24:23 +0000

    Exercise 3

--- exercise_3.rb
+++ exercise_3.rb
@@ -4,8 +4,26 @@
 #      quilt -> iltquay
 #      qat   -> atqay
 
+word = " "
+first_letter = " "
 def pig_latin(word)
+  if word[0] == "q" && word[1] == "u"
+    word.slice!(0)
+    word.slice!(0)
+    word + "quay"
 
+  elsif word[0] == "a" || word[0] == "e" || word[0] == "i" || word[0] == "o" || word[0] == "u"
+     word + "way"
+
+  else
+    first_letter = word[0]
+    while first_letter != "a" and word[0] != "e" and word[0] != "i" and word[0] != "o" and word[0] != "u"
+      word.slice!(0)
+      word = word + first_letter
+      first_letter = word[0]
+    end
+      word + "ay"
+  end
 end
 
 ## Tests:
